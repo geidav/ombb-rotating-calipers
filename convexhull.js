acd88e6b6bf5bf009cae09102ece63f3f8b0a5dd
David Geier <geidav@googlemail.com>
2014-06-10 12:30:54 +0200

    fixed infinite loop in Gift Wrapping implementation

--- convexhull.js
+++ convexhull.js
@@ -1,13 +1,14 @@
-var EPS    = 0.0001;
+"use strict";
 
-var ON    = 0;
-var LEFT  = 1;
+var ON = 0;
+var LEFT = 1;
 var RIGHT = 2;
+var ALMOST_ZERO = 0.00001;
 
 function GetSideOfLine(lineStart, lineEnd, point)
 {
     var d = (lineEnd.x-lineStart.x)*(point.y-lineStart.y)-(lineEnd.y-lineStart.y)*(point.x-lineStart.x);
-    return (d > EPS ? LEFT : (d < -EPS ? RIGHT : ON));
+    return (d > ALMOST_ZERO ? LEFT : (d < -ALMOST_ZERO ? RIGHT : ON));
 }
 
 // returns convex hull in CW order
@@ -19,26 +20,32 @@ function CalcConvexHull(points)
         return points;
 
     // find first hull point
-    var hullPt = points[0].clone();
+    var hullPt = points[0];
     var convexHull = [];
     
     for (var i=1; i<points.length; i++)
+    {
+        // perform lexicographical compare
         if (points[i].x < hullPt.x)
             hullPt = points[i];
-         
+        else if (Math.abs(points[i].x-hullPt.x) < ALMOST_ZERO) // equal
+            if (points[i].y < hullPt.y)
+                hullPt = points[i];
+    }
+        
     // find remaining hull points
     do
-    {
-        convexHull.unshift(hullPt);
+    {        
+        convexHull.unshift(hullPt.clone());
         var endPt = points[0];
-        
+       
         for (var j=1; j<points.length; j++)
         {           
             var side = GetSideOfLine(hullPt, endPt, points[j]);
             
             // in case point lies on line take the one further away.
             // this fixes the collinearity problem.
-            if (side == LEFT || (side == ON && (hullPt.distance(points[j]) > hullPt.distance(endPt))))
+            if (endPt.equals(hullPt) || (side == LEFT || (side == ON && hullPt.distance(points[j]) > hullPt.distance(endPt))))
                 endPt = points[j];
         }
        
